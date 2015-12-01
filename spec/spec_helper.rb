b7c9d5d49fea30f479d1e2cc476d7ec88c3bdd2d
Colin Saliceti <c.saliceti@gmail.com>
2015-12-01 16:32:55 +0000

    rspec tests for bosh-init
    It runs basic tests for packages, permissions, version...
    The constants are defined in the spec helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,10 @@ require 'serverspec'
 
 set :backend, :docker
 set :os, family: :debian
+
+# bosh-init
+
+BOSH_INIT_PACKAGES = "build-essential zlibc zlib1g-dev ruby ruby-dev openssl libxslt1-dev libxml2-dev \
+    libssl-dev libreadline6 libreadline6-dev libyaml-dev libsqlite3-dev sqlite3"
+BOSH_INIT_BIN = "/usr/local/bin/bosh-init"
+BOSH_INIT_VERSION = "0.0.80-a62aad7-2015-10-28T01:52:30Z"
