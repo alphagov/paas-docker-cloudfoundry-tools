117fd3397e50ef1ba7a4c786fdc3164f6c93715e
Colin Saliceti <c.saliceti@gmail.com>
2015-12-03 13:53:04 +0000

    spruce container tests
    We can't change the docker image in serverspec to run 2 different tests in the same run.
    It is now changed to run 2 independent tasks instead.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,8 @@ BOSH_INIT_PACKAGES = "build-essential zlibc zlib1g-dev ruby ruby-dev openssl lib
     libssl-dev libreadline6 libreadline6-dev libyaml-dev libsqlite3-dev sqlite3"
 BOSH_INIT_BIN = "/usr/local/bin/bosh-init"
 BOSH_INIT_VERSION = "0.0.80-a62aad7-2015-10-28T01:52:30Z"
+
+# spruce
+
+SPRUCE_BIN = "/go/bin/spruce"
+SPRUCE_VERSION = "0.12.0"
---
+++ spec/spruce/spruce_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+require 'docker'
+require 'serverspec'
+
+describe "spruce image" do
+  before(:all) {
+    @image = Docker::Image.all().detect{|i| i.info['RepoTags'].include? 'spruce:latest' }
+    set :docker_image, @image.id
+  }
+
+  it "should be available" do
+    expect(@image).to_not be_nil
+  end
+
+  it "installs the right version of Alpine Linux" do
+    expect(os_version).to include("Alpine Linux 3.2")
+  end
+
+  def os_version
+    command("cat /etc/issue | head -1").stdout
+  end
+
+  it "checks if spruce binary exists and is a file" do
+    expect(file(SPRUCE_BIN)).to be_file
+  end
+
+  it "checks if spruce binary is executable" do
+    expect(file(SPRUCE_BIN)).to be_mode 755
+  end
+
+  it "has the spruce version #{SPRUCE_VERSION}" do
+    expect(spruce_version).to eq("spruce - Version #{SPRUCE_VERSION} (master)")
+  end
+
+  def spruce_version
+    command("spruce --version").stderr.strip
+  end
+end
