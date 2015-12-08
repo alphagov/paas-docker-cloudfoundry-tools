f6b5cec04317f69838cb2ad3e7c73e647803c2fd
Piotr Komborski <piotr.komborski@gmail.com>
2015-12-08 17:22:23 +0000

    New tests for Alipne linux
    As we are no longer using Ubuntu we had to adjust tests to reflect new
    envirnoment.

--- spec/terraform_spec.rb
+++ spec/terraform_spec.rb
@@ -10,27 +10,27 @@ end
         expect(@image).to_not be_nil
     end
 
-    it "installs the right version of Ubuntu" do
-        expect(os_version).to include("Ubuntu 14")
-    end
-
-    def os_version
-        command("lsb_release -a").stdout
-    end
-
-    it "installs openssh-client" do
-        expect(package("openssh-client")).to be_installed
+    it "installs the Alpine linux" do
+         expect(file("/etc/alpine-release")).to be_file
     end
 
     it "checks if terraform binary is executable" do
-         expect(file("/usr/local/bin/terraform")).to be_mode 775
+         expect(file("/usr/local/bin/terraform")).to be_mode 755
     end
 
-    it "has the Terraform version 0.6.8" do
-        expect(terraform_version).to include("Terraform v0.6.8")
+    it "has the Terraform version 0.6.9" do
+        expect(terraform_version).to include("Terraform v0.6.9")
     end
 
     def terraform_version
         command("terraform version").stdout.strip
+    end
+
+    it "installs SSH" do
+        expect(ssh_version).to include("OpenSSH")
+    end
+
+    def ssh_version
+        command("ssh -V").stderr.strip
     end
 end
