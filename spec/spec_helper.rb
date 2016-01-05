cdd1579b9af644b669285bd2e987207b01abb730
Michal Tekel <mtekel@gmail.com>
2016-01-05 17:44:44 +0000

    Find images by suffix rather than exact match
    When you build image locally, it's called just `image`. When you do
    `docker pull org/image`, as docker instructs you on the docker hub page,
    image will be called `org/image`. Find images by `image` suffix, rather
    than exact match.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,7 +20,7 @@ SPRUCE_VERSION = "0.13.0"
 CURL_SSL_PACKAGES = "curl openssl ca-certificates"
 
 def find_image_id(name)
-  image = Docker::Image.all().detect{ |i| i.info['RepoTags'].include? name }
+  image = Docker::Image.all().detect{|i| i.info['RepoTags'][0].end_with? name }
   raise "Docker image '#{name}' not found. You may need to run 'rake build:<name>' first" unless image
 
   image.id
