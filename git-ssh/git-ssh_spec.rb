abbeaa0af4d2cc39de46580dd17bba1051137ab9
Colin Saliceti <c.saliceti@gmail.com>
2016-03-21 17:00:41 +0000

    Update openssh in git-ssh
    The openssh-client 7.1p2 version was removed from the index and this
    was breaking the whole build.
    We choose now to not pin the version of individual packages as:
    
    * the versions change too quickly
    * there is not much value in pinning down to individual packages
    * there is additional burden for security fixes
    
    The version of Alpine is pinned, but we don't test against it to avoid
    maintenance burden.

--- git-ssh/git-ssh_spec.rb
+++ git-ssh/git-ssh_spec.rb
@@ -4,19 +4,15 @@ require 'serverspec'
 
 PACKAGES = "git openssh-client ca-certificates"
 GIT_VERSION = "2.6.4"
-OPENSSH_VERSION = "7.1p2"
+OPENSSH_VERSION = "7.2p2"
 
 describe "image" do
   before(:all) {
     set :docker_image, find_image_id('git-ssh:latest')
   }
 
-  it "installs the right version of Alpine" do
-    expect(os_version).to include("Alpine Linux 3.3")
-  end
-
-  def os_version
-    command("cat /etc/issue | head -1").stdout
+  it "installs Alpine" do
+    expect(command("cat /etc/issue | head -1").stdout).to include("Alpine Linux")
   end
 
   it "installs required packages" do
@@ -29,16 +25,8 @@ describe "image" do
     expect(command('git --version').exit_status).to eq(0)
   end
 
-  it "has the right git version" do
-    expect(command("git --version").stdout.strip).to include(GIT_VERSION)
-  end
-
   it "can run ssh" do
     expect(command('ssh -V').exit_status).to eq(0)
-  end
-
-  it "has the right ssh version" do
-    expect(command("ssh -V").stderr.strip).to include(OPENSSH_VERSION)
   end
 
 end
