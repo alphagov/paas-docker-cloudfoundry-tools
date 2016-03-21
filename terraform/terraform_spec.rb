475306bcbf5655b3bb95e9b9ad85eb172e0d6ff1
Colin Saliceti <c.saliceti@gmail.com>
2016-03-21 17:02:35 +0000

    Pin Alpine terraform version
    We pin the version to guarantee some stability but we don't test
    for that version in specs to avoid to maintain the version in
    2 places.

--- terraform/terraform_spec.rb
+++ terraform/terraform_spec.rb
@@ -19,8 +19,8 @@ describe "Terraform image" do
     set :docker_image, find_image_id('terraform:latest')
   }
 
-  it "installs the Alpine linux" do
-    expect(file("/etc/alpine-release")).to be_file
+  it "installs Alpine" do
+    expect(command("cat /etc/issue | head -1").stdout).to include("Alpine Linux")
   end
 
   it "installs Root Certificates" do
