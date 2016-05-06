7709dba84d0133ae8894e03659a446e35a4cf766
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2016-05-06 10:13:03 +0100

    Make notice about CPI deploy error more visible
    When building the container, the `bosh-init deploy` task errors with a
    big red error message.  There's a warning above indicating that this is
    expected, but it's easy to miss.  Making it red will make it more
    obvious.

--- bosh-init/bosh_init_cache/seed_bosh_init_cache.sh
+++ bosh-init/bosh_init_cache/seed_bosh_init_cache.sh
@@ -9,10 +9,15 @@ cd "${WORKING_DIR}"
 
 sed "s|BOSH_AWS_CPI_URL|${BOSH_AWS_CPI_URL}|; s|BOSH_AWS_CPI_CHECKSUM|${BOSH_AWS_CPI_CHECKSUM}|" minimal_in.yml > minimal.yml
 
+RED='\033[0;31m'
+NO_COLOUR='\033[0m'
+
+echo "${RED}"
 echo "###############################################"
 echo "# Compiling AWS CPI release...                #"
 echo "# The deploy will error, but that's expected. #"
 echo "###############################################"
+echo "${NO_COLOUR}"
 
 set +e
 bosh-init deploy minimal.yml
