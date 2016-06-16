a2dbc945025233ebddb177b298031d430f7162c7
Michal Tekel <mtekel@gmail.com>
2016-06-16 12:57:41 +0100

    Upgrade spruce to 1.4.5
    Spruce now provides static builds, which enables us to run it from
    Alpine (see https://github.com/geofffranks/spruce/issues/128).
    
    Use new spruce binary to reduce container size by 80%:
     * old 73MB
     * new 14MB

--- spruce/spruce_spec.rb
+++ spruce/spruce_spec.rb
@@ -2,8 +2,9 @@ require 'spec_helper'
 require 'docker'
 require 'serverspec'
 
-SPRUCE_BIN = "/go/bin/spruce"
-SPRUCE_VERSION = "0.13.0"
+SPRUCE_BIN = "/usr/local/bin/spruce"
+SPRUCE_VERSION = "1.4.5"
+ALPINE_VERSION = "3.4"
 
 describe "spruce image" do
   before(:all) {
@@ -11,7 +12,7 @@ describe "spruce image" do
   }
 
   it "installs the right version of Alpine Linux" do
-    expect(os_version).to include("Alpine Linux 3.2")
+    expect(os_version).to include("Alpine Linux #{ALPINE_VERSION}")
   end
 
   def os_version
