8e1448f5f04fddfd686e8cc24ca5aea61ebf7b23
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2016-10-03 17:27:06 +0100

    Prepare custom terraform build
    We have modified both, `terraform` and the `go-datadog-api` packages,
    enabling simple addition of two desired keys `style` and `type` to
    the graph implementation for each dashboard.
    This has been planned out, as part of the
    [#131428567 - Add options to datadog terraform provider][1].
    
    This should allow us, to continue with the
    [#120590777 - Improve Visibility of Continuous Smoke Tests][2] story.
    
    [1]: https://www.pivotaltracker.com/story/show/131428567
    [2]: https://www.pivotaltracker.com/story/show/120590777

--- terraform/terraform_spec.rb
+++ terraform/terraform_spec.rb
@@ -18,7 +18,7 @@ describe "Terraform image" do
   it "has the expected Terraform version" do
     expect(
       command("terraform version").stdout
-    ).to include("Terraform v0.7.3")
+    ).to include("Terraform v0.7.5-dev")
   end
 
   it "installs SSH" do
