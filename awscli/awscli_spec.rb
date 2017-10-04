f53502ccf80232569d7f2a851882f1eebc2d8325
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2017-10-04 12:18:54 +0100

    Upgrade AWS CLI tool to the latest version
    We'd like to use some functionality (`start-db-instance` and
    `stop-db-instance`) in our pipeline. The current version does not cover
    these.
    
    We're upgrading to the latest available version on pip as frankly, we
    don't see any reason not to.

--- awscli/awscli_spec.rb
+++ awscli/awscli_spec.rb
@@ -5,7 +5,7 @@ require 'serverspec'
 AWSCLI_PACKAGES = "curl openssl ca-certificates less"
 
 AWSCLI_BIN = "/usr/bin/aws"
-AWSCLI_VERSION = "1.10.38"
+AWSCLI_VERSION = "1.11.164"
 
 describe "awscli image" do
   before(:all) {
