fd6a600743ecec1b373f64e7e7aacc52de7016ac
Olivier ORAND <olivier.orand@orange.com>
2018-01-05 16:48:42 +0100

    customize terraform image for orange
    we include all required providers to avoid download on first run

--- terraform/README.md
+++ terraform/README.md
@@ -1,7 +1,9 @@
-[![Build Status](https://travis-ci.org/alphagov/paas-docker-terraform.svg)](https://travis-ci.org/alphagov/paas-docker-terraform)
+Orange: [![Build Status](https://travis-ci.org/orange-cloudfoundry/paas-docker-cloudfoundry-tools.svg)](https://travis-ci.org/orange-cloudfoundry/paas-docker-cloudfoundry-tools)
+AlphaGov: [![Build Status](https://travis-ci.org/alphagov/paas-docker-cloudfoundry-tools.svg)](https://travis-ci.org/alphagov/paas-docker-cloudfoundry-tools)
+
 # docker-terraform
 
-This container allows you to run Terraform inside a Docker container.
+This container allows you to run Terraform inside a Docker container with plugins already downloaded.
 
 ## How to run and build
 
--- terraform/terraform_spec.rb
+++ terraform/terraform_spec.rb
@@ -5,6 +5,7 @@ require 'serverspec'
 describe "Terraform image" do
   before(:all) {
     set :docker_image, find_image_id('terraform:latest')
+    @providers_count = 0
   }
 
   it "installs Alpine" do
@@ -18,7 +19,7 @@ describe "Terraform image" do
   it "has the expected Terraform version" do
     expect(
       command("terraform version").stdout
-    ).to include("Terraform v0.8.5")
+    ).to include("Terraform v0.10.2")
   end
 
   it "installs SSH" do
@@ -32,4 +33,51 @@ describe "Terraform image" do
       Integer(command("du -m /usr/local/bin").stdout.split.first)
     ).to be < 200
   end
+
+  context "providers checks" do
+    let(:terraform_shared_dir) { File.join('', 'root','.terraform.d','providers','linux_amd64') }
+    # minus 2 to exclude . and ..
+    let(:expected_terraform_providers_count) { Dir.entries(terraform_shared_dir).length-2 }
+
+    it "has the terraform provider" do
+      expect(file(File.join(terraform_shared_dir,'terraform-provider-terraform_v1.0.2_x4'))).to be_file
+      @providers_count += 1
+    end
+
+    it "has the cloudflare provider" do
+      expect(file(File.join(terraform_shared_dir,'terraform-provider-cloudflare_v0.1.0_x4'))).to be_file
+      @providers_count += 1
+    end
+
+    it "has the local provider" do
+      expect(file(File.join(terraform_shared_dir,'terraform-provider-local_v1.0.0_x4'))).to be_file
+      @providers_count += 1
+    end
+
+    it "has the openstack provider" do
+      expect(file(File.join(terraform_shared_dir,'terraform-provider-openstack_v1.1.0_x4'))).to be_file
+      @providers_count += 1
+    end
+
+    it "has the powerdns provider" do
+      expect(file(File.join(terraform_shared_dir,'terraform-provider-powerdns_v0.1.0_x4'))).to be_file
+      @providers_count += 1
+    end
+
+    it "has the credhub provider" do
+      expect(file(File.join(terraform_shared_dir,'terraform-provider-credhub_0.10_linux_amd64'))).to be_file
+      @providers_count += 1
+    end
+
+    it "has the cloudfoundry provider" do
+      expect(file(File.join(terraform_shared_dir,'terraform-provider-cloudfoundry_0.10_linux_amd64'))).to be_file
+      @providers_count += 1
+    end
+
+    it "has enough providers" do
+      expect(@providers_count).to eq(expected_terraform_providers_count)
+    end
+
+  end
+
 end
