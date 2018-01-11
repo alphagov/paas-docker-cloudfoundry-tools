663235c17a11f6a3e98fc4a315287edb39abe836
Olivier ORAND <olivier.orand@orange.com>
2018-01-11 10:46:26 +0100

    update local provider to 1.1.0

--- terraform/plugin_cache.tf
+++ terraform/plugin_cache.tf
@@ -1,7 +1,6 @@
-provider "terraform" {version = "~> 1.0.2"}
 provider "openstack" {version = "~> 1.1.0"}
 provider "cloudflare" {version = "~> 0.1.0"}
-provider "local" { version = "~> 1.0.0"}
+provider "local" { version = "~> 1.1.0"}
 provider "powerdns" {version = "~> 0.1.0"}
 
 # Orange provider
