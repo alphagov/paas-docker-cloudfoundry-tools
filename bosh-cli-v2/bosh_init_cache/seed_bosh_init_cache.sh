d2df4652a8f83b9dd9465b8b9ac715720d2194f5
bandesz <bandesz@gmail.com>
2018-02-19 16:02:29 +0000

    Prepare the Bosh CLI v2 image to create an environment
    We are migrating from CLI version v1 to v2 which means we want to use e.g.
    `bosh create-env` instead of the `bosh-init` command.
    The necessary dependencies are defined in [1] and to keep the maintenance
    easier we change the base image to Ubuntu 16.04.
    
    We updated the Bosh CLI to 2.0.48 so we can use the latest version.
    
    A dummy `bosh create-env` command will run at the end of the build to cache
    some resources to speed up the create process. This was copied from the
    bosh-init Docker image.
    
    [1] https://bosh.io/docs/cli-env-deps.html

---
+++ bosh-cli-v2/bosh_init_cache/seed_bosh_init_cache.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+set -u
+
+WORKING_DIR=$(cd $(dirname "${0}") && pwd)
+
+cd "${WORKING_DIR}"
+
+sed "s|BOSH_AWS_CPI_URL|${BOSH_AWS_CPI_URL}|; s|BOSH_AWS_CPI_CHECKSUM|${BOSH_AWS_CPI_CHECKSUM}|" minimal_in.yml > minimal.yml
+
+RED='\033[0;31m'
+NO_COLOUR='\033[0m'
+
+echo "${RED}"
+echo "###############################################"
+echo "# Compiling AWS CPI release...                #"
+echo "# The deploy will error, but that's expected. #"
+echo "###############################################"
+echo "${NO_COLOUR}"
+
+set +e
+bosh create-env minimal.yml
+set -e
+
+echo "...done"
