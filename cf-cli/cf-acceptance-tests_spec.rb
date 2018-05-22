8faff4cbb9d7f0815eaf6cae4723f4c1d7061d11
bandesz <bandesz@gmail.com>
2018-05-22 16:33:11 +0100

    Update Ruby version to 2.5 for the cf-cli Docker image
    Ruby 2.2 reached EOL on 2018-03-31 (#1), so we decided to migrate to the latest
    stable version.
    
    Originally the image used the alpine:3.3 base image which had an old version of
    Ruby (2.2.*). We changed the base image to ruby:2.5-alpine as it seems to fit
    better.

--- cf-cli/cf-acceptance-tests_spec.rb
+++ cf-cli/cf-acceptance-tests_spec.rb
@@ -51,10 +51,10 @@ describe "cf-cli image" do
     expect(cmd.stdout).to match(/^jq-1.5/)
   end
 
-  it "has ruby 2.2 available" do
+  it "has ruby 2.5 available" do
     cmd = command("ruby -v")
     expect(cmd.exit_status).to eq(0)
-    expect(cmd.stdout).to match(/^ruby 2.2/)
+    expect(cmd.stdout).to match(/^ruby 2.5/)
   end
 
   it "has ruby json gem available" do
