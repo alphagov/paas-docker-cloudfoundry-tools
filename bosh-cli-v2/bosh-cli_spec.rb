f0a576d4724b2a65966286479ccb80ccbb22bd31
Hector Rivas Gandara <keymon@gmail.com>
2018-09-19 14:51:11 +0100

    Update bosh-cli-v2 to 5.2.1 with SKI for certs
    The version 5.2.1 [1] supports generating certificates via
    --vars-store with SKI/AKI.
    
    We need this to be able to rotate CA certificates used by CF API,
    otherwise it is not possible to use two CA certs with the same name
    at the same time.
    
    More info in [2]
    
    [1] https://github.com/cloudfoundry/bosh-cli/releases/tag/v5.2.1
    [2] https://github.com/cloudfoundry/config-server/pull/9

--- bosh-cli-v2/bosh-cli_spec.rb
+++ bosh-cli-v2/bosh-cli_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'docker'
 require 'serverspec'
 
-BOSH_CLI_VERSION="3.0.1-712bfd7-2018-03-13T23:26:43Z"
+BOSH_CLI_VERSION="5.2.1-24101936-2018-08-27T21:55:34Z"
 
 BOSH_ENV_DEPS = "build-essential zlibc zlib1g-dev openssl libxslt1-dev libxml2-dev \
     libssl-dev libreadline7 libreadline-dev libyaml-dev libsqlite3-dev sqlite3"
