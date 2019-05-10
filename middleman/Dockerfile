d2a3dc9ecd075e9f3a68a416276b298958f6b411
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2019-05-10 16:10:24 +0100

    Add an image that contains middleman
    We use https://middlemanapp.com/ to build things like the team manual
    and the tech docs.
    
    The team manual sometimes takes almost an hour to build:
    
    https://concourse.build.ci.cloudpipeline.digital/teams/main/pipelines/paas-team-manual/jobs/publish/builds/95
    
    Because it's doing an apt-get update / install every time it builds the docs.
    
    If we had a better base image than ruby:2.5-slim (like one that already
    had the other dependencies) we'd be able to skip the apt-get bits and
    just jump in at `bundle install && bundle exec middleman build`.
    
    For good measure I've installed middleman in this image directly, but in
    practice we'd probably want the apps to be able to pick their own
    version with bundler.

---
+++ middleman/Dockerfile
@@ -0,0 +1,3 @@
+FROM ruby:2.5-slim
+
+RUN apt-get update && apt-get install -y build-essential libffi-dev git libcurl4-openssl-dev nodejs && gem install middleman
---
+++ middleman/middleman_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require 'docker'
+require 'serverspec'
+
+describe "middleman image" do
+  before(:all) {
+    set :docker_image, find_image_id('middleman:latest')
+  }
+
+  it "can run middleman" do
+    expect(command('middleman --help').exit_status).to eq(0)
+  end
+
+end
