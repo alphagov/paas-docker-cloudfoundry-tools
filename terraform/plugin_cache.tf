b13f4d99687f74e93c88f46bae431fa4bcf67b0d
Lee Porte <lee.porte@digital.cabinet-office.gov.uk>
2020-08-10 12:28:18 +0100

    Update terraform version to 0.12.29 and update plugins

--- terraform/plugin_cache.tf
+++ terraform/plugin_cache.tf
@@ -1,12 +1,8 @@
 provider "aws" {
-  version = "2.30.0"
+  version = "2.70.0"
 }
-provider "datadog" {
-  version = "1.0.3"
-}
-provider "template" {
-  version = "1.0.0"
-}
+
 provider "null" {
-  version = "1.0.0"
+  version = "~> 2.1"
 }
+
