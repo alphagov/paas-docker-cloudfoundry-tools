1d1b3491ce3a9c4148fc824320729e4d9ec2b6a3
Lee Porte <lee.porte@digital.cabinet-office.gov.uk>
2021-04-19 08:26:22 +0100

    Update Alpine to use 3.13
    The version of Alpine used in the containers is outdated. This commit
    updates them to the latest current version.

--- spruce/spruce_spec.rb
+++ spruce/spruce_spec.rb
@@ -4,7 +4,7 @@ require 'serverspec'
 
 SPRUCE_BIN = "/usr/local/bin/spruce"
 SPRUCE_VERSION = "1.27.0"
-ALPINE_VERSION = "3.12"
+ALPINE_VERSION = "3.13"
 
 describe "spruce image" do
   before(:all) {
