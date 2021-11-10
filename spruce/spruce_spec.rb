3c07361f3ecf2d526ca34de561731756b0e2a02a
Olivier ORAND <olivier.orand@orange.com>
2021-11-10 11:52:13 +0100

    [bosh-cli-v2|cf-cli|spruce]: cli bump for bosh, cf credhub and spruce
    - bosh cli: [6.4.11](https://github.com/cloudfoundry/bosh-cli/releases/tag/v6.4.11)
    - cf cli: [7.4.0](https://github.com/cloudfoundry/cli/releases/tag/v7.4.0)
       - cf cli: [7.3.0](https://github.com/cloudfoundry/cli/releases/tag/v7.3.0) [SAN required]
    - credhub cli: [2.9.1](https://github.com/cloudfoundry/credhub-cli/releases/tag/2.9.1)
    - spruce: [1.29](https://github.com/geofffranks/spruce/releases/tag/v1.29.0)
    
    [SAN required]: https://github.com/cloudfoundry/routing-release/blob/develop/docs/golang1.15-remove-x509ignoreCN%3D0-flag-certificates-now-require-SANs.md

--- spruce/spruce_spec.rb
+++ spruce/spruce_spec.rb
@@ -3,7 +3,7 @@ require 'docker'
 require 'serverspec'
 
 SPRUCE_BIN = "/usr/local/bin/spruce"
-SPRUCE_VERSION = "1.27.0"
+SPRUCE_VERSION = "1.29.0"
 ALPINE_VERSION = "3.13"
 
 describe "spruce image" do
