850793200532307ad3c8099a6ff8091477bdf940
Harsh Vasudev <harsh.vasudev1@digital.cabinet-office.gov.uk>
2024-07-26 16:25:53 +0100

    Upgrade terraform version

--- terraform/terraform_spec.rb
+++ terraform/terraform_spec.rb
@@ -26,7 +26,7 @@ describe "Terraform image" do
   it "has the expected Terraform version" do
     expect(
       command("terraform version").stdout
-    ).to match("Terraform v1.5.2")
+    ).to match("Terraform v1.9.2")
   end
 
   it "installs SSH" do
